0e077193dc2de8e48a6490ccfb648489101c4a84
MaxRyazan <no4ta87@mail.ru>
2023-01-01 12:22:57 +0300

    v1.3 animation was added, modal wind was added

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 <script>
 import MainView from "@/views/MainView";
 import '@/css/styles.scss'
+import '@/css/vueAnimation.scss'
 export default {
     components: {MainView}
 }
--- src/components/AppTable.vue
+++ src/components/AppTable.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="one_table_container table_right">
         <app-table-header/>
-        <div v-for="detail in details" :key="detail.id">
+        <transition-group name="fade">
+            <div v-for="detail in details" :key="detail.id">
             <div class="table_first_row">
                 <div class=" cell id">{{detail.id}}</div>
                 <div class=" cell">{{detail.name}}</div>
@@ -12,13 +13,15 @@
                     <div class="mark" v-for="mark in detail.carMarks" :key="mark">{{ mark }}</div>
                 </div>
                 <div class="inline description">
-                    <div class="cell ">{{detail.description}}</div>
-                    <img v-if="inOrder" src="@/assets/images/arrow.png" alt=""
+                    <div class="cell" @click="openModal = true">{{detail.description}}</div>
+                    <img v-if="inOrder"
+                         src="@/assets/images/arrow.png" alt=""
                          class="select"
                          title="В заказ"
                          @click="$emit('addToOrder', detail.id)"
                     >
-                    <img v-else src="@/assets/images/cross.png" alt=""
+                    <img v-else
+                         src="@/assets/images/cross.png" alt=""
                          class="select"
                          title="В заказ"
                          @click="$emit('deleteFromOrder', detail.id)"
@@ -30,14 +33,58 @@
                 <app-button  v-if="!inOrder">Оформить</app-button>
             </div>
         </div>
+        </transition-group>
+        <div class="modal" v-if="openModal">
+            <button class="close_modal" @click="openModal = false">x</button>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dicta fugit maiores perspiciatis rem.
+            Aspernatur assumenda, blanditiis explicabo facilis fuga magni minima quidem veniam. Assumenda cum distinctio
+            dolor dolore dolores fuga, provident? Aliquid eaque eum explicabo possimus! Accusantium
+            aliquid dolor doloribus exercitationem fugiat hic inventore odit repudiandae, sequi? Corporis, mollitia!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dicta fugit maiores perspiciatis rem.
+            Aspernatur assumenda, blanditiis explicabo facilis fuga magni minima quidem veniam. Assumenda cum distinctio
+            dolor dolore dolores fuga, provident? Aliquid eaque eum explicabo possimus! Accusantium
+            aliquid dolor doloribus exercitationem fugiat hic inventore odit repudiandae, sequi? Corporis, mollitia!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dicta fugit maiores perspiciatis rem.
+            Aspernatur assumenda, blanditiis explicabo facilis fuga magni minima quidem veniam. Assumenda cum distinctio
+            dolor dolore dolores fuga, provident? Aliquid eaque eum explicabo possimus! Accusantium
+            aliquid dolor doloribus exercitationem fugiat hic inventore odit repudiandae, sequi? Corporis, mollitia!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dicta fugit maiores perspiciatis rem.
+            Aspernatur assumenda, blanditiis explicabo facilis fuga magni minima quidem veniam. Assumenda cum distinctio
+            dolor dolore dolores fuga, provident? Aliquid eaque eum explicabo possimus! Accusantium
+            aliquid dolor doloribus exercitationem fugiat hic inventore odit repudiandae, sequi? Corporis, mollitia!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dicta fugit maiores perspiciatis rem.
+            Aspernatur assumenda, blanditiis explicabo facilis fuga magni minima quidem veniam. Assumenda cum distinctio
+            dolor dolore dolores fuga, provident? Aliquid eaque eum explicabo possimus! Accusantium
+            aliquid dolor doloribus exercitationem fugiat hic inventore odit repudiandae, sequi? Corporis, mollitia!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dicta fugit maiores perspiciatis rem.
+            Aspernatur assumenda, blanditiis explicabo facilis fuga magni minima quidem veniam. Assumenda cum distinctio
+            dolor dolore dolores fuga, provident? Aliquid eaque eum explicabo possimus! Accusantium
+            aliquid dolor doloribus exercitationem fugiat hic inventore odit repudiandae, sequi? Corporis, mollitia!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dicta fugit maiores perspiciatis rem.
+            Aspernatur assumenda, blanditiis explicabo facilis fuga magni minima quidem veniam. Assumenda cum distinctio
+            dolor dolore dolores fuga, provident? Aliquid eaque eum explicabo possimus! Accusantium
+            aliquid dolor doloribus exercitationem fugiat hic inventore odit repudiandae, sequi? Corporis, mollitia!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dicta fugit maiores perspiciatis rem.
+            Aspernatur assumenda, blanditiis explicabo facilis fuga magni minima quidem veniam. Assumenda cum distinctio
+            dolor dolore dolores fuga, provident? Aliquid eaque eum explicabo possimus! Accusantium
+            aliquid dolor doloribus exercitationem fugiat hic inventore odit repudiandae, sequi? Corporis, mollitia!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dicta fugit maiores perspiciatis rem.
+            Aspernatur assumenda, blanditiis explicabo facilis fuga magni minima quidem veniam. Assumenda cum distinctio
+            dolor dolore dolores fuga, provident? Aliquid eaque eum explicabo possimus! Accusantium
+            aliquid dolor doloribus exercitationem fugiat hic inventore odit repudiandae, sequi? Corporis, mollitia!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dicta fugit maiores perspiciatis rem.
+            Aspernatur assumenda, blanditiis explicabo facilis fuga magni minima quidem veniam. Assumenda cum distinctio
+            dolor dolore dolores fuga, provident? Aliquid eaque eum explicabo possimus! Accusantium
+            aliquid dolor doloribus exercitationem fugiat hic inventore odit repudiandae, sequi? Corporis, mollitia!
+        </div>
     </div>
 </template>
-
 <script setup>
-import {defineEmits, defineProps} from 'vue'
+import {defineEmits, defineProps, ref} from 'vue'
 import AppButton from '@/components/AppButton'
 import AppTableHeader from "@/components/AppTableHeader";
 
+let openModal = ref(false)
 defineEmits(['addToOrder'])
 defineProps({
     details:{
